97540c791817e477789547caadb5eb509def9e05
Alec von Barnekow <von.barnekow.alec@gmail.com>
2018-11-11 21:24:32 +0100

    Add logs

---
+++ src/log.ts
@@ -0,0 +1,13 @@
+import * as path from 'path';
+import * as winston from 'winston';
+
+export const log = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  transports: [
+    new winston.transports.File({ filename: path.join('logs/' , 'error.log'), level: 'error' }),
+    new winston.transports.File({ filename: path.join('logs/' , 'info.log'), level: 'info' }),
+    new winston.transports.File({ filename: path.join('logs/' , 'warning.log'), level: 'warning' }),
+    new winston.transports.File({ filename: path.join('logs/' , 'combined.log')}),
+  ],
+});
